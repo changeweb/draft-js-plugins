624e52345aff887ffd786bcc17aacb56454e51cf
Hasib Mahmud <kishorpasha870@yahoo.com>
2016-07-01 22:50:44 +0600

    Anchor tag add test

--- draft-js-hashtag-plugin/src/Hashtag/index.js
+++ draft-js-hashtag-plugin/src/Hashtag/index.js
@@ -5,8 +5,13 @@ export default class Hashtag extends Component {
   render() {
     const { theme = {}, className, ...props } = this.props; // eslint-disable-line no-use-before-define
     const combinedClassName = unionClassNames(theme.hashtag, className);
+    
+    var decoratedText = this.props.decoratedText === undefined ? '' : this.props.decoratedText;
+    var href = "/hashtag/" + decoratedText;
     return (
-      <span {...props} className={combinedClassName} />
+      <a href={href} target="_blank">
+        <span {...props} className={combinedClassName} />
+      </a>
     );
   }
 }
