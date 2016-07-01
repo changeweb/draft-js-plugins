a02b8b894f7465d43dcb5e861b423822dd1360db
Hasib Mahmud <kishorpasha870@yahoo.com>
2016-07-01 23:43:08 +0600

    Update index.js

--- draft-js-hashtag-plugin/src/Hashtag/index.js
+++ draft-js-hashtag-plugin/src/Hashtag/index.js
@@ -3,13 +3,12 @@ import unionClassNames from 'union-class-names';
 
 export default class Hashtag extends Component {
   render() {
-    const { theme = {}, className, ...props } = this.props; // eslint-disable-line no-use-before-define
+    const { decoratedText = '', theme = {}, target = '_self', className, ...props } = this.props; // eslint-disable-line no-use-before-define
     const combinedClassName = unionClassNames(theme.hashtag, className);
     
-    var decoratedText = this.props.decoratedText === undefined ? '' : this.props.decoratedText;
-    var href = "/hashtag/" + decoratedText;
+    const href = "/hashtag/" + decoratedText;
     return (
-      <a href={href} target="_blank">
+      <a {...props} href={href} target={target}>
         <span {...props} className={combinedClassName} />
       </a>
     );
